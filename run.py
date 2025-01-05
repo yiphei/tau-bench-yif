a77d33e2cf59306989c8d0259c5d89af8a17bdc2
yifei yan <yanyiphei@gmail.com>
2025-01-05 15:07:08 -0800

    added changes

--- run.py
+++ run.py
@@ -10,12 +10,16 @@ import multiprocessing
 from typing import List, Dict, Any
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict
 
 from tau_bench.envs import get_env
 from tau_bench.agents.base import Agent
 from tau_bench.types import EnvRunResult
 from litellm import provider_list
 from tau_bench.envs.user import UserStrategy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def run(
@@ -45,64 +49,76 @@ def run(
     else:
         print(
             f"Running tasks {args.start_index} to {end_index} (checkpoint path: {ckpt_path})"
-    )
-    for i in range(args.num_trials):
-        if args.task_ids and len(args.task_ids) > 0:
-            idxs = args.task_ids
-        else:
-            idxs = list(range(args.start_index, end_index))
-        if args.shuffle:
-            random.shuffle(idxs)
+        )
 
-        def _run(idx: int) -> EnvRunResult:
-            isolated_env = get_env(
-                args.env,
-                user_strategy=args.user_strategy,
-                user_model=args.user_model,
-                task_split=args.task_split,
-                user_provider=args.user_model_provider,
+    if args.task_ids and len(args.task_ids) > 0:
+        idxs = args.task_ids
+    else:
+        idxs = list(range(args.start_index, end_index))
+    if args.shuffle:
+        random.shuffle(idxs)
+    idx_to_trial = defaultdict(lambda: -1)
+    idxs = idxs * args.num_trials
+
+    def _run(idx: int) -> EnvRunResult:
+        isolated_env = get_env(
+            args.env,
+            user_strategy=args.user_strategy,
+            user_model=args.user_model,
+            task_split=args.task_split,
+            user_provider=args.user_model_provider,
+            task_index=idx,
+        )
+
+        idx_to_trial[idx] += 1
+        trial = idx_to_trial[idx]
+
+        print(f"Running task {idx}")
+        try:
+            res = agent.solve(
+                env=isolated_env,
                 task_index=idx,
             )
-
-            print(f"Running task {idx}")
-            try:
-                res = agent.solve(
-                    env=isolated_env,
-                    task_index=idx,
-                )
-                result = EnvRunResult(
-                    task_id=idx,
-                    reward=res.reward,
-                    info=res.info,
-                    traj=res.messages,
-                    trial=i,
-                )
-            except Exception as e:
-                result = EnvRunResult(
-                    task_id=idx,
-                    reward=0.0,
-                    info={"error": str(e), "traceback": traceback.format_exc()},
-                    traj=[],
-                    trial=i,
-                )
-            print(
-                "✅" if result.reward == 1 else "❌",
-                f"task_id={idx}",
-                result.info,
+            result = EnvRunResult(
+                task_id=idx,
+                reward=res.reward,
+                info=res.info,
+                key_actions=res.key_actions,
+                traj=res.messages,
+                trial=trial,
+                raw_messages=res.raw_messages,
+                node_turns=res.node_turns,
+                oai_messages=res.oai_messages,
+                anthropic_messages=res.anthropic_messages,
             )
-            print("-----")
-            with lock:
-                data = []
-                if os.path.exists(ckpt_path):
-                    with open(ckpt_path, "r") as f:
-                        data = json.load(f)
-                with open(ckpt_path, "w") as f:
-                    json.dump(data + [result.model_dump()], f, indent=2)
-            return result
+        except Exception as e:
+            result = EnvRunResult(
+                task_id=idx,
+                reward=0.0,
+                info={"error": str(e), "traceback": traceback.format_exc()},
+                traj=[],
+                trial=trial,
+            )
+            print("TRACEBACK")
+            print(traceback.format_exc())
+        print(
+            "✅" if result.reward == 1 else "❌",
+            f"task_id={idx}",
+            result.info,
+        )
+        print("-----")
+        with lock:
+            data = []
+            if os.path.exists(ckpt_path):
+                with open(ckpt_path, "r") as f:
+                    data = json.load(f)
+            with open(ckpt_path, "w") as f:
+                json.dump(data + [result.model_dump()], f, indent=2)
+        return result
 
-        with ThreadPoolExecutor(max_workers=args.max_concurrency) as executor:
-            res = list(executor.map(_run, idxs))
-            results.extend(res)
+    with ThreadPoolExecutor(max_workers=args.max_concurrency) as executor:
+        res = list(executor.map(_run, idxs))
+        results.extend(res)
 
     return results
 
@@ -115,6 +131,17 @@ def agent_factory(
         from tau_bench.agents.tool_calling_agent import ToolCallingAgent
 
         return ToolCallingAgent(
+            tools_info=tools_info,
+            wiki=wiki,
+            model=args.model,
+            provider=args.model_provider,
+            temperature=args.temperature,
+        )
+    elif args.agent_strategy == "custom":
+        # native tool calling
+        from tau_bench.agents.custom_tool_calling_agent import CustomToolCallingAgent
+
+        return CustomToolCallingAgent(
             tools_info=tools_info,
             wiki=wiki,
             model=args.model,
@@ -145,19 +172,6 @@ def agent_factory(
             use_reasoning=True,
             temperature=args.temperature,
         )
-    elif args.agent_strategy == "few-shot":
-        from tau_bench.agents.few_shot_agent import FewShotToolCallingAgent
-        with open(args.few_shot_displays_path, "r") as f:
-            few_shot_displays = [json.loads(line)["messages_display"] for line in f]
-
-        return FewShotToolCallingAgent(
-            tools_info=tools_info,
-            wiki=wiki,
-            model=args.model,
-            provider=args.model_provider,
-            few_shot_displays=few_shot_displays,
-            temperature=args.temperature,
-        )
     else:
         raise ValueError(f"Unknown agent strategy: {args.agent_strategy}")
 
@@ -184,8 +198,17 @@ def display_metrics(results: List[EnvRunResult]) -> None:
         pass_hat_ks[k] = sum_task_pass_hat_k / len(c_per_task_id)
     print(f"🏆 Average reward: {avg_reward}")
     print("📈 Pass^k")
+    metric_dict = {
+        "total_rewards": sum(rewards),
+        "num_trials": num_trials,
+        "avg_reward": avg_reward,
+        "pass_k": {},
+    }
     for k, pass_hat_k in pass_hat_ks.items():
         print(f"  k={k}: {pass_hat_k}")
+        metric_dict["pass_k"][k] = pass_hat_k
+
+    return metric_dict
 
 
 def main():
@@ -221,7 +244,7 @@ def main():
         "--agent-strategy",
         type=str,
         default="tool-calling",
-        choices=["tool-calling", "act", "react", "few-shot"],
+        choices=["tool-calling", "act", "react", "custom"],
     )
     parser.add_argument(
         "--temperature",
@@ -238,7 +261,12 @@ def main():
     )
     parser.add_argument("--start-index", type=int, default=0)
     parser.add_argument("--end-index", type=int, default=-1, help="Run all tasks if -1")
-    parser.add_argument("--task-ids", type=int, nargs="+", help="(Optional) run only the tasks with the given IDs")
+    parser.add_argument(
+        "--task-ids",
+        type=int,
+        nargs="+",
+        help="(Optional) run only the tasks with the given IDs",
+    )
     parser.add_argument("--log-dir", type=str, default="results")
     parser.add_argument(
         "--max-concurrency",
@@ -248,8 +276,13 @@ def main():
     )
     parser.add_argument("--seed", type=int, default=10)
     parser.add_argument("--shuffle", type=int, default=0)
-    parser.add_argument("--user-strategy", type=str, default="llm", choices=[item.value for item in UserStrategy])
-    parser.add_argument("--few-shot-displays-path", type=str, help="Path to a jsonlines file containing few shot displays")
+    parser.add_argument(
+        "--user-strategy",
+        type=str,
+        default="llm",
+        choices=[item.value for item in UserStrategy],
+    )
+
     args = parser.parse_args()
     print(args)
     random.seed(args.seed)
@@ -265,10 +298,10 @@ def main():
         ckpt_path=file_str,
     )
 
-    display_metrics(results)
-
+    metric_dict = display_metrics(results)
+    metric_dict["results"] = [result.model_dump() for result in results]
     with open(file_str, "w") as f:
-        json.dump([result.model_dump() for result in results], f, indent=2)
+        json.dump(metric_dict, f, indent=2)
         print(f"\n📄 Results saved to {file_str}\n")
 
 
